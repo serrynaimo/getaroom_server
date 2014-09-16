b02effc3fdd3c3f148b50fd2147e7b3051fc75a9
Thomas Gorissen <serrynaimo@me.com>
2014-09-16 12:07:07 +0800

    Updating server

---
+++ config/dev.js
@@ -0,0 +1,11 @@
+exports = {
+    aws: {
+      region: 'ap-southeast-1',
+      apiVersion: '2010-03-31',
+      accessKeyId: '',
+      secretAccessKey: '',
+      platform: 'android',
+      platformApplicationArn: ''
+    },
+    sendFromEmail: 'your@email.com'
+}
--- server.js
+++ server.js
@@ -1,25 +1,124 @@
+// Require modules
 var express = require('express'),
-    crypto = require('crypto-js'),
     cors = require('cors'),
-    XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+    redis = require('redis'),
+    nodemailer = require('nodemailer'),
+    ses = require('nodemailer-ses-transport'),
+    sns = require('sns-mobile'),
+    extend = require('extend'),
+    config = require('config.js');
 
-    app = exports.app = express();
+// Configuration and Constants
+var emailPattern = /^[a-z0-9_+.-]+@[a-z0-9.-]+\.[a-z0-9]{2,}$/;
+
+// Transport initilization
+var notificationTransport = new sns(config.aws),
+    mailTransport = nodemailer.createTransport(ses(extend(config.aws, {
+        region: 'us-west-2', // ap-southeast-1 not supported
+        rateLimit: 5
+    }))),
+    client = redis.createClient();
+
+// Helper functions
+var decodeId = function (id) {
+        if(!id || id.length < 5)
+            return "";
+        id = id.replace("-", "/").replace("_","+") + "=";
+        return new Buffer(id, 'base64').toString('ascii');
+    },
+    sendEmail = function(caller, callee, room, callback) {
+        mailTransport.sendMail({
+            from: caller + ' <' + config.sendFromEmail + '>',
+            replyTo: caller,
+            to: callee,
+            subject: "Join me on a video call right now",
+            text: "Hey there,\n\n" + caller + " is waiting for you on getaroom.io to be joined for a video call. It's a free tool and you don't need to sign up or install anything. Just follow this link and put pants on ;)\n\nhttp://getaroom.io/" + room + "\n\n"
+        }, callback);
+    },
+    sendNotification = function(caller, endpointArn, room, callback) {
+        notificationTransport.sendMessage(endpointArn,
+            '{"ADM": "{\\"time_to_live\\": 60, \\"data\\": {\\"message\\": \\"' + caller + ' is calling ...\\",\\"room\\": \\"' + room + '\\"}}"}',
+            callback);
+    }
+
+// Event subscriptions
+client.on("error", function(err) {
+    console.log("Redis error: " + err);
+});
+
+// App/Express settings
+app = exports.app = express();
 
 app.set('port', process.env.PORT || 8001)
     .set('env', process.env.NODE_ENV || 'local');
 
 app.use(cors());
 
+// Endpoint definitions
+app.get('/register', function (req, res) {
+    console.log("Register Request: " + JSON.stringify(req.query));
 
-
-app.get('/authorize', function (req, res) {
-
-
+    if(req.query.id && req.query.device) {
+        notificationTransport.addUser(req.query.device, null, function(err, endpointArn) {
+            if(err) {
+              console.log("SNS Error:" + err);
+              return res.status(500).send('SNS Error');
+            }
+            client.set(req.query.id, endpointArn, redis.print);
+            console.log("Saved '" + req.query.device + "' with endpoint " + endpointArn);
+            res.status(200).send('OK');
+        });
+    }
+    else {
+        res.status(400).send('Bad Request');
+    }
 
 });
 
+app.get('/call', function (req, res) {
+    console.log("Call Request: " + JSON.stringify(req.query));
 
+    var caller = decodeId(req.query.caller),
+        callee = decodeId(req.query.callee);
 
+    if(!caller.match(emailPattern) || !callee.match(emailPattern)) {
+        res.status(400).send('Bad Request');
+        return;
+    }
+
+    client.get(req.query.callee, function(err, endpointArn){
+
+        var mailCallback = function(err) {
+                if(err) {
+                    console.log('SES Error: Email could not be delivered to ' + callee + '. ' + err);
+                    res.status(500).send('Server Error');
+                }
+                else {
+                    console.log("Sent Email to " + callee);
+                    res.status(200).send('OK');
+                }
+            },
+            notificationCallback = function(err, messageId) {
+                if(err) {
+                  console.log('SNS Error: Notification could not be delivered to ' + endpointArn + '. ' + err);
+                  sendEmail(caller, callee, req.query.caller, mailCallback);
+                } else {
+                  console.log("Sent Notification from " + caller + " to " + endpointArn);
+                  res.status(200).send('OK');
+                }
+            };
+
+        if(endpointArn) {
+            sendNotification(caller, endpointArn, req.query.caller, notificationCallback);
+        }
+        else {
+            sendEmail(caller, callee, req.query.caller, mailCallback);
+        }
+    });
+
+});
+
+// Startup
 app.listen(app.get('port'));
 
 console.log('Get a room Server ' + app.get('env') +
