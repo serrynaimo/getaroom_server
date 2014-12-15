d38aa1824ddc9a144fd25e1d2e0275dca8440732
Liang Xiangrong <xiangrong@temasys.com.sg>
2014-12-15 22:30:44 +0800

    AFWA-57 WIP: Added invite actions on App server. Invite works. Need to implement other client invite actions to test. Major changes:  - Improved error messages for /call API.  - Added enum package.  - Added Enum:   -- RegStatus    --- User's registration status, including state of database record   -- ActionType    --- Type of API action to be performed.   -- CloudType    --- Type of cloud notification, e.g. ADM, GCM.  - sendEmail to include ActionType  - Create sendCloud by merging sendADM and sendGCM   -- takes CloudType to use appropriate cloud.   -- takes ActionType to send appropriate message.

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // Require modules
 var express = require('express'),
     cors = require('cors'),
+    Enum = require('enum'),
     redis = require('redis'),
     nodemailer = require('nodemailer'),
     ses = require('nodemailer-ses-transport'),
@@ -34,6 +35,36 @@ var admTransport = new sns(config.aws),
     // { userId: Encoded email, cloud: ADM/GCM, endpointArn: ADM/GCM registration ID }
     client = redis.createClient();
 
+// Enums
+  // User's registration status, including state of database record:
+var RegStatus = new Enum ({
+  // Possible status:
+    // callee is registered with proper record in database.
+    'REGISTERED': 0,
+    // callee is not registered, i.e. not found in database.
+    'UNREGISTERED': 1,
+    // callee is registered but redis has problem retrieving record.
+    'RECORD_ERROR': 2,
+});
+  // Type of API action to be performed.
+var ActionType = new Enum ({
+  // Possible types:
+    // Caller is making the invite.
+  'INVITE_INVITE': 'inviteInvite',
+    // Caller is cancelling the invite.
+  'INVITE_CANCEL': 'inviteCancel',
+    // Callee has accepted invite.
+  'INVITE_ACCEPT': 'inviteAccept',
+    // Callee has declined invite.
+  'INVITE_DECLINE': 'inviteDecline',
+});
+  // Type of cloud notification, e.g. ADM, GCM.
+var CloudType = new Enum ({
+  // Possible types:
+  'ADM': 'ADM',
+  'GCM': 'GCM',
+});
+
 // Helper functions
 var decodeId = function (id) {
         if(!id || id.length <= 6)
@@ -41,60 +72,80 @@ var decodeId = function (id) {
         id = id.replace("-", "/").replace("_","+") + "=";
         return new Buffer(id, 'base64').toString('ascii');
     },
-    sendEmail = function(caller, callee, room, callback) {
+    getEnumFromValue = function( enumArray, val ) {
+      // Go through array of enum and return the one that matches value.
+      for( var i = 0, len = enumArray.length; i < len; ++i ) {
+        if( enumArray[ i ].value == val )
+          return enumArray[ i ];
+      }
+      return undefined;
+    },
+    sendEmail = function( caller, callee, room, callback, actionType ) {
+      var emailText;
+      switch( actionType ) {
+        case ActionType.INVITE_INVITE:
+          emailText = 
+            "Hey there,\n\n" + caller + " is waiting for you on " + config.domain + 
+            " to be joined for a video call. It's a free tool and you don't need to sign up " + 
+            "or install anything. Just follow this link and put pants on ;)\n\n" +
+            "http://" + config.domain + "/" + room + "\n\n";
+          break;
+        case ActionType.INVITE_CANCEL:
+          emailText = caller + " has canceled the video call they started.";
+          break;
+        case ActionType.INVITE_ACCEPT:
+          emailText = callee + " has accepted the video call you started.";
+          break;
+        case ActionType.INVITE_DECLINE:
+          emailText = callee + " has declined the video call you started.";
+          break;
+      }
         mailTransport.sendMail({
             from: caller + ' <' + config.sendFromEmail + '>',
             replyTo: caller,
             to: callee,
             subject: "Join me on a video call right now",
-            text: "Hey there,\n\n" + caller + " is waiting for you on " + config.domain + " to be joined for a video call. It's a free tool and you don't need to sign up or install anything. Just follow this link and put pants on ;)\n\nhttp://" + config.domain + "/" + room + "\n\n"
+            text: emailText
         }, callback);
     },
-    sendADM = function( emailCaller, endpointArn, room, emailCallee, callback ) {
-      // Create ADM message in JSON
-      var notification = {
-        default: emailCaller + ' is calling. Go to http://' + config.domain + '/' + room + ' to accept the call.',
-        ADM: JSON.stringify({
-          data: {
-            message: emailCaller + ' is calling ...',
-            room: room,
-            emailCaller: emailCaller,
-            emailCallee: emailCallee
-          },
-          expiresAfter: 60
-        }),
-        GCM: JSON.stringify({
-          data: {
-            message: emailCaller + ' is calling ...',
-            room: room,
-            emailCaller: emailCaller,
-            emailCallee: emailCallee
-          },
-          time_to_live: 60
-        })
+    sendCloud = function( emailCaller, endpointArn, room, emailCallee, callback, cloud, action ) {
+      var dataCloud = {
+        message: emailCaller + ' is calling ...',
+        room: room,
+        emailCaller: emailCaller,
+        emailCallee: emailCallee,
+        inviteType: action.value,
       };
-      // Send message via ADM.
-      admTransport.sendMessage(endpointArn, notification, callback);
-    },
-    // Send a message via GCM
-    sendGCM = function( emailCaller, endpointArn, room, emailCallee, callback ) {
-      // Create GCM message in JSON.
-      var message = new gcm.Message({
-        collapseKey: 'Invitation',
-        // delayWhileIdle: true,
-        data: {
-          message: emailCaller + ' is calling ...',
-          room: room,
-          emailCaller: emailCaller,
-          emailCallee: emailCallee
-        },
-        timeToLive: 60
-      });
-      // Create sender list
-      var registrationIds = [];   
-      registrationIds.push( endpointArn );
-      // Send message via GCM
-      gcmTransport.send( message, registrationIds, 4, callback );
+      switch ( cloud ) {
+        case CloudType.ADM:
+          // Create ADM message in JSON
+          var notification = {
+            default: emailCaller + ' is calling. Go to http://' + config.domain + '/' + room + ' to accept the call.',
+            ADM: JSON.stringify({
+              data: dataCloud,
+              expiresAfter: 60
+            }),
+          };
+          // Send message via ADM.
+          admTransport.sendMessage(endpointArn, notification, callback);
+          break;
+        case CloudType.GCM:
+          // Create GCM message in JSON.
+          var message = new gcm.Message({
+            collapseKey: 'Invitation',
+            // delayWhileIdle: true,
+            data: dataCloud,
+            timeToLive: 60
+          });
+          // Create sender list
+          var registrationIds = [];   
+          registrationIds.push( endpointArn );
+          // Send message via GCM
+          gcmTransport.send( message, registrationIds, 4, callback );
+          break;
+        default:
+          console.log( 'Unknown cloud type used: ' + cloud.toString() );
+      }
     },
     // Log with util.inspect, which returns a string representation of object.
       // Shows non-enumerable properties.
@@ -103,7 +154,7 @@ var decodeId = function (id) {
     logUI = function( logObject ) {
       console.log( util.inspect(
        logObject, { showHidden: true, depth: null, colors: true } ) );
-    }
+    };
 
 // Event subscriptions
 client.on("error", function(err) {
@@ -115,6 +166,7 @@ app.get('/register', function (req, res) {
   var userId = req.query.id,
       regId = req.query.device,
       cloud = req.query.cloud,
+      cloudType = getEnumFromValue( CloudType.enums, cloud ),
       emailUser = decodeId( userId );
 
   // A valid reqister call must have all 3 values.
@@ -128,30 +180,27 @@ app.get('/register', function (req, res) {
 
     // Add user to database
       // ADM
-    if( cloud == 'ADM' ) {
+    if( cloudType == CloudType.ADM ) {
       admTransport.addUser(req.query.device, null, function(err, endpointArn) {
         if(err) {
           console.log("SNS Error:" + err);
           return res.status(500).send('SNS Error');
         }
         // Create user object
-        var user = { userId: userId, cloud: cloud, endpointArn: endpointArn };
+        var user = { userId: userId, cloud: cloudType.value, endpointArn: endpointArn };
         client.set( userId, JSON.stringify( user ), redis.print );
-
-        console.log("Registered " + cloud + " device '" + req.query.device +
-          "' with endpoint " + endpointArn);
-        res.status(200).send('OK');
+        logUI( 'User object is\n:' + JSON.stringify( user ) );
       });
-    } else if( cloud == 'GCM' ) {
+    } else if( cloudType == CloudType.GCM ) {
       // GCM
         // Create user object
-      var user = { userId: userId, cloud: cloud, endpointArn: regId };
+      var user = { userId: userId, cloud: cloudType.value, endpointArn: regId };
       client.set( userId, JSON.stringify( user ), redis.print );
-
-      console.log( "Registered user " + emailUser + " with id " + userId + ", via " + cloud + " device token and endpoint: "
-       + regId );
-      res.status(200).send('OK');
+      logUI( 'User object is\n:' + JSON.stringify( user ) );
     }
+    console.log( "Registered user " + emailUser + " with id " + userId + ", via " + cloud + 
+      " device token and endpoint: " + regId );
+    res.status(200).send('OK');
   }
   else {
     res.status(400).send('Bad Request');
@@ -161,82 +210,120 @@ app.get('/register', function (req, res) {
 app.get('/call', function (req, res) {
   var idCaller = req.query.caller,
       idCallee = req.query.callee,
-      actionType = req.query.action,
+      action = req.query.action,
+      actionType = getEnumFromValue( ActionType.enums, action ),
       emailCaller = decodeId( idCaller ),
       emailCallee = decodeId( idCallee );
 
+  // Check for proper parameters
+    // Check email formats
   if(!emailCaller.match(emailPattern) || !emailCallee.match(emailPattern)) {
-    res.status(400).send( 'Bad Request. Ensure you have id, device, cloud.' );
+    var errStr = 
+      'Bad Request. Ensure you have encoded a valid email address for both caller and callee.';
+    res.status(400).send( errStr );
+    console.log( errStr );
+    return;
+  }
+    // Check actionType defined.
+  if( typeof( actionType ) == 'undefined' ) {
+    var errStr = 'Bad Request. Ensure you have included an action parameter.\n' + 
+    'action: ' + action + '\nactionType: ' + actionType;
+    res.status( 400 ).send( errStr );
+    console.log( errStr );
     return;
   }
 
-  if( actionType == 'inviteCancel' ) {
-    // Caller is cancelling the invite.
-    // Push notification to Callee.
-    
-  } else if( actionType == 'inviteAccept' ) {
-    // Callee has accepted invite.
-    // Push notification to Caller.
-    
-  } else if( actionType == 'inviteDecline' ) {
-    // Callee has declined invite.
-    // Push notification to Caller.
-    
-  } else {
-    client.get(idCallee, function( err, userStr ){
-      // Email is sent for following cases:
-        // callee is not registered, i.e. not found in database.
-        // callee is registered but redis has problem retrieving record.
-        // sending of notification failed, for e.g. if registration id is wrong.
-      var mailCallback = function(err) {
-        if(err) {
-          console.log('SES Error: Email could not be delivered to ' + emailCallee + '. ' + err);
-          res.status(500).send('Server Error');
-        }
-        else {
-          console.log("Sent Email to " + emailCallee);
-          res.status(200).send('OK');
-        }
-      };
+  // Check registration status of callee.
+  var regStatus,
+      user;
 
-      if( !err ) {
-      // If callee is registered (can be found in database) and successfully retrieved.
-        if( userStr != 'nil' ) {
-          var user = JSON.parse( userStr );
-          var cloud = user.cloud;
-          var endpointArn = user.endpointArn;
+  client.get( idCallee, function( err, userStr ){
+    // Callee may have following registration status:
+      // callee is registered with proper record in database.
+      // callee is not registered, i.e. not found in database.
+      // callee is registered but redis has problem retrieving record.
 
-          var notificationCallback = function(err, messageId) {
-            if(err) {
-              console.log( 'SNS Error: ' + cloud + ' notification from ' + emailCaller + 
-                '\ncould not be delivered to ' + emailCallee + 
-                '\nat device endpoint ' + endpointArn + '.\n' + err );
-              sendEmail( emailCaller, emailCallee, idCaller, mailCallback );
-            } else {
-              console.log( "Sent Notification from " + emailCaller + " to " + emailCallee + 
-                '\nat device endpoint ' + endpointArn + '\nmessageId:\n' );
-              // 2nd parameter (messageId) in callback for may contain canonical id(s) for GCM,
-                // If the current device id used is not the latest registered device id.
-              logUI( messageId );
-              res.status(200).send('OK');
-            }
-          };
-
-          // Send to the right cloud
-          if( cloud == 'ADM')
-            sendADM(emailCaller, endpointArn, idCaller, emailCallee, notificationCallback);
-          else if( cloud == 'GCM' )
-            sendGCM(emailCaller, endpointArn, idCaller, emailCallee, notificationCallback);
-        } else {
-          // callee is not registered
-          sendEmail( emailCaller, emailCallee, idCaller, mailCallback );
-        }
+    if( !err ) {
+    // If callee is registered (can be found in database) and successfully retrieved.
+      if( userStr != 'nil' ) {
+        regStatus = RegStatus.REGISTERED;
+        user = JSON.parse( userStr );
+        logUI( 'RegStatus: Callee userStr found in db:\n' + userStr );
+        logUI( 'RegStatus: Callee user object:\n' + user );
       } else {
-        // callee is registered but redis has problem retrieving record,
-          // e.g. if record is not a string.
-        sendEmail( emailCaller, emailCallee, idCaller, mailCallback );
+        // callee is not registered
+        regStatus = RegStatus.UNREGISTERED;
+        console.log( 'RegStatus: Callee NOT found in db.' );
       }
-    });
+    } else {
+      // callee is registered but redis has problem retrieving record,
+        // e.g. if record is not a string.
+      regStatus = RegStatus.RECORD_ERROR;
+      console.log( 'RegStatus: Callee found in db, but there is a problem retrieving record:\n' +
+        err );
+    }
+
+    // Process call request.
+    processCallAction( actionType );
+  });
+
+  // Send notification depending on action request.
+  var processCallAction = function( actionType ){
+    // Email is sent for following cases:
+      // callee is registered.
+      // callee is not registered, i.e. not found in database.
+      // callee is registered but redis has problem retrieving record.
+      // sending of notification failed, for e.g. if registration id is wrong.
+    var mailCallback = function(err) {
+      if(err) {
+        console.log('SES Error: Email could not be delivered to ' + emailCallee + '. ' + err);
+        res.status(500).send('Server Error');
+      }
+      else {
+        console.log("Sent Email to " + emailCallee);
+        res.status(200).send('OK');
+      }
+    };
+    switch( regStatus ) {
+      case RegStatus.REGISTERED:
+        // If callee is registered (can be found in database) and successfully retrieved.
+        var cloud = user.cloud;
+        var cloudType = getEnumFromValue( CloudType.enums, cloud );
+        var endpointArn = user.endpointArn;
+
+        var notificationCallback = function( err, messageId ) {
+          if(err) {
+            console.log( 'SNS Error: ' + cloud + ' notification from ' + emailCaller + 
+              '\ncould not be delivered to ' + emailCallee + 
+              '\nat device endpoint ' + endpointArn + '.\n' + err );
+            sendEmail( emailCaller, emailCallee, idCaller, mailCallback, actionType );
+          } else {
+            console.log( "Sent Notification from " + emailCaller + " to " + emailCallee + 
+              '\nat device endpoint ' + endpointArn + '\nmessageId:\n' );
+            // 2nd parameter (messageId) in callback for may contain canonical id(s) for GCM,
+              // If the current device id used is not the latest registered device id.
+            logUI( messageId );
+            // Send email in any case.
+            sendEmail( emailCaller, emailCallee, idCaller, mailCallback, actionType );
+          }
+        };
+        // Send to the right cloud
+        sendCloud( emailCaller, endpointArn, idCaller, emailCallee, notificationCallback,
+          cloudType, actionType );
+        break;
+      case RegStatus.UNREGISTERED:
+          // callee is not registered
+      case RegStatus.RECORD_ERROR:
+          // callee is registered but redis has problem retrieving record,
+            // e.g. if record is not a string.
+          sendEmail( emailCaller, emailCallee, idCaller, mailCallback, actionType );
+        break;
+      default:
+        // Unknown registration status.
+        console.log( "Tried to send Notification from " + emailCaller + " to " + emailCallee + 
+          '\nat device endpoint ' + endpointArn + ',\nbut Callee has unknown regStatus: ' +
+          regStatus.toString() + '.' );
+    }
   }
 });
 
